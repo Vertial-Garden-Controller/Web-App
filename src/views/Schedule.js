899cc51484086f4f0b6cddd5425b254a93962e2f
Joshua Barringer <jbarringer1999@gmail.com>
2021-05-12 00:02:29 -0700

    api call setup for retrieving schedule information

--- src/views/Schedule.js
+++ src/views/Schedule.js
@@ -2,73 +2,36 @@ import { Fragment, useEffect, useState } from "react";
 
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import Loading from "../components/Loading";
-// import axios from 'axios'
+import axios from 'axios'
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const Schedule = () => {
-    // const [plantJSON, setPlantJSON] = useState()
-    // const [value, setValue] = useState('ALL')
-    // const [currentPlant, setCurrentPlant] = useState()
+    const [scheduleJSON, setScheduleJSON] = useState()
+    const {
+        user,
+      } = useAuth0();
 
     useEffect(() => {
-        // if(!plantJSON) {
-        //     async function fetchData() {
-        //         const response = await axios.get('http://localhost:5001/plant_types')
-        //         setPlantJSON(response.data.plant_types)
-        //         setCurrentPlant(Object.values(response.data.plant_types))
-        //         console.log(response.data.plant_types)
-        //     }
-        //     fetchData()
-        // }
-    }, [])
-
+        if(!scheduleJSON) {
+            async function fetchData() {
+                const response = await axios.get(`http://localhost:5001/schedule/user/?email=${user.email}`)
+                setScheduleJSON(response.data.schedules)
+                console.log(response.data.schedules)
+            }
+            fetchData()
+        }
+    }, [scheduleJSON, user.email])
+    
     return (
         <Fragment>
-            <h1>Schedule Page</h1>
-            {/* {
-                !plantJSON ?
-                <div>No Plant Data is Currently Available</div> :
+            <h1>Watering Schedules</h1>
+            {
+                !scheduleJSON ?
+                <div>No Schedule Data is Currently Available</div> :
                 <div>
-                    {
-                        <div>
-                            <select value={value} onChange={e => {
-                                setValue(e.currentTarget.value)
-                                if(e.currentTarget.value !== 'ALL') {
-                                    setCurrentPlant([Object.values(plantJSON).find(
-                                        f => f.name === e.currentTarget.value
-                                    )])
-                                } else {
-                                    setCurrentPlant(Object.values(plantJSON))
-                                }
-                            }}>
-                                <option value={'ALL'}>ALL</option>
-                                {Object.values(plantJSON).map((plant) => (
-                                    <option value={plant.name}>{plant.name}</option>
-                                ))}
-                            </select>
-                            {
-                                currentPlant ?
-                                Object.values(currentPlant).map((plant) => (
-                                    <div>
-                                        <hr />
-                                        <h2>{plant.name}</h2>
-                                        <p>Water {plant.gallons_per_week} gallon(s) weekly.</p>
-                                        {
-                                            plant.early_stage ? 
-                                            <p>! - This plant needs extra water during early growth.</p> :
-                                            <div />
-                                        }
-                                        <p>{plant.flavor}</p>
-                                    </div>
-                                )) :
-                                <p>Select a Plant</p>
-                                
-                            }
-                        </div>
-                        }
-                    </div>
-                
-                
-            } */}
+                    <p>{scheduleJSON[0].start_time}</p>
+                </div>
+            }
         </Fragment>
     );
 };
